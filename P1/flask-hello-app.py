adef52ed587c1df7d2459d231375b24077167f8e
Blake Gould <gouldbf@gmail.com>
2020-12-18 05:53:37 -0800

    Fix False nullable property of name.

--- P1/flask-hello-app.py
+++ P1/flask-hello-app.py
@@ -9,7 +9,7 @@ db = SQLAlchemy(app)
 class Person(db.Model):
     __tablename__ = 'persons'
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(), nullable=FALSE)
+    name = db.Column(db.String(), nullable=False)
 
 @app.route('/')
 def index():
