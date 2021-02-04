270cc9bcc1360fff40cd735d97059b7b07bce2f9
Blake Gould <gouldbf@gmail.com>
2021-02-04 07:48:08 -0700

    P1 L5.2 - start the todo app

---
+++ todoapp/app.py
@@ -0,0 +1,14 @@
+from flask import Flask,render_template
+
+
+app = Flask(__name__) #creates app named after the name of the file
+
+@app.route('/')
+def index():
+    return render_template('index.html', data=[{
+        'description': 'Todo 1'
+    }, {
+        'description': 'Todo 2'
+    }, {
+        'description': 'Todo 3'
+    }])
